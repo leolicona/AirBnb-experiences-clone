1b9b013bba259ec1998c86950c980659a00bc9a3
leolicona <leolicona.dev@gmail.com>
2022-03-10 12:00:00 -0600

    Heare and Navbar componentents was created

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
+import "./styles.css"
+
+export default function App() {
+    return(
+        <div>
+            <Navbar/>
+            <Hero/>
+            <Card/>
+        </div>
+    )
+}
