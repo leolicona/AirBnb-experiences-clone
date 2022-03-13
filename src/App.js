7392e956e4763a7eae4df3bab8295d34f54d48a2
leolicona <leolicona.dev@gmail.com>
2022-03-13 11:04:14 -0600

    Props spread objet as a prop

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
          const CardElements = cardsData.map( (data) => {
             return <Card
                key={data.id}
-               data={data}
+               {...data}
             />    
     })
     
--- src/components/Card.js
+++ src/components/Card.js
@@ -4,24 +4,24 @@ import React from "react"
 
 export default function Card (props) {
     let badgeText
-    if (props.data.openSpots === 0) {
+    if (props.openSpots === 0) {
         badgeText = "SOLD OUT"
-    } else if (props.data.location === "Online") {
+    } else if (props.location === "Online") {
         badgeText = "ONLINE"
     } 
    
     return(
         <div className="card">
             {badgeText && <div className="card--badge">{badgeText}</div>}
-            <img className="card--image" src={`./images/${props.data.coverImg}`} alt="Card"/>
+            <img className="card--image" src={`./images/${props.coverImg}`} alt="Card"/>
             <div className="card--stats">
                 <img src="./images/star.svg" alt="star"/>
-                <span>{props.data. stats.rating}</span>
-                <span className="gray">({props.data. stats.reviewCount}) -</span>
-                <span className="gray">{props.data.location}</span>
+                <span>{props.stats.rating}</span>
+                <span className="gray">({props.stats.reviewCount}) -</span>
+                <span className="gray">{props.location}</span>
             </div>
-            <p className="card--title">{props.data.title}</p>
-            <p className="card--price"><span className="bold">From $ {props.data.price} </span> / person</p>
+            <p className="card--title">{props.title}</p>
+            <p className="card--price"><span className="bold">From $ {props.price} </span> / person</p>
 
         </div>
     )
